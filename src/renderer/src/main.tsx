eb9544d4a74c2ba659045b26e9be699ab84590ce
gbu <zhaobutter@gmail.com>
2026-06-19 20:50:46 +0800

    移除广告、推广类链接，Excalidraw links下的内容

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Excalidraw } from "@excalidraw/excalidraw";
+import { Excalidraw, MainMenu } from "@excalidraw/excalidraw";
 import "@excalidraw/excalidraw/index.css";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <main className="app-shell">
-      <Excalidraw />
+      <Excalidraw>
+        <MainMenu>
+          <MainMenu.DefaultItems.LoadScene />
+          <MainMenu.DefaultItems.SaveToActiveFile />
+          <MainMenu.DefaultItems.Export />
+          <MainMenu.DefaultItems.SaveAsImage />
+          <MainMenu.DefaultItems.SearchMenu />
+          <MainMenu.DefaultItems.Help />
+          <MainMenu.DefaultItems.ClearCanvas />
+          <MainMenu.Separator />
+          <MainMenu.DefaultItems.ToggleTheme />
+          <MainMenu.DefaultItems.ChangeCanvasBackground />
+        </MainMenu>
+      </Excalidraw>
     </main>
   </React.StrictMode>
 );
